91ef2a0f9d772dee81cb7305f966b4b95d084e02
FiorenNathasia <fiorennathasia@gmail.com>
2025-05-28 22:55:10 +0100

    Update add entry page

--- src/pages/AddEntry/AddEntry.jsx
+++ src/pages/AddEntry/AddEntry.jsx
@@ -2,6 +2,15 @@ import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Canvas from "../../components/Canvas/Canvas";
+import {
+  Container,
+  Typography,
+  Box,
+  Button,
+  Stack,
+  TextField,
+  CircularProgress,
+} from "@mui/material";
 
 function AddEntry() {
   const [title, setTitle] = useState("");
@@ -40,39 +49,100 @@ function AddEntry() {
   };
 
   return (
-    <>
-      <div className="addentry">
-        <div className="addentry__container">
-          <button onClick={back}>x</button>
-          <h1>How are you feeling?</h1>
-          <input
-            className="addentry__title"
-            type="text"
+    <Container maxWidth="md" sx={{ paddingTop: 5 }}>
+      <Box mb={3}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          flexDirection="row"
+          mb={3}
+          width="100%"
+          alignItems="flex-start"
+        >
+          <Typography variant="h4">How are you feeling?</Typography>
+        </Box>
+      </Box>
+      <Stack direction="column" gap={3} width="100%" mt={2} mb={2}>
+        <Box>
+          <Typography variant="caption" color="textSecondary">
+            Title
+          </Typography>
+          <TextField
+            fullWidth
+            variant="standard"
             value={title}
-            placeholder="Title"
             onChange={(e) => setTitle(e.target.value)}
-            disabled={isSaving}
-          ></input>
-          <Canvas canvasRef={canvasRef} />
-          <textarea
-            className="addentry__text"
-            type="text"
+          />
+        </Box>
+
+        <Canvas canvasRef={canvasRef} />
+
+        <Box>
+          <Typography variant="caption" color="textSecondary">
+            Body
+          </Typography>
+          <TextField
+            variant="outlined"
+            fullWidth
+            multiline
             value={text}
-            placeholder="Text"
+            minRows={4}
             onChange={(e) => setText(e.target.value)}
-            disabled={isSaving}
-          ></textarea>
-          <button
-            className="addentry__submit"
-            onClick={handleSubmit}
-            disabled={isSaving}
-          >
-            {isSaving ? "Saving..." : "Save"}
-          </button>
-        </div>
-      </div>
-    </>
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: 3,
+              },
+            }}
+          />
+        </Box>
+      </Stack>
+      <Button variant="contained" onClick={handleSubmit}>
+        Save
+        {isSaving && (
+          <CircularProgress
+            sx={{ marginLeft: 1 }}
+            size="1rem"
+            color="secondary"
+          />
+        )}
+      </Button>
+    </Container>
   );
+
+  // return (
+  //   <>
+  //     <div className="addentry">
+  //       <div className="addentry__container">
+  //         <button onClick={back}>x</button>
+  //         <h1>How are you feeling?</h1>
+  //         <input
+  //           className="addentry__title"
+  //           type="text"
+  //           value={title}
+  //           placeholder="Title"
+  //           onChange={(e) => setTitle(e.target.value)}
+  //           disabled={isSaving}
+  //         ></input>
+  //         <Canvas canvasRef={canvasRef} />
+  //         <textarea
+  //           className="addentry__text"
+  //           type="text"
+  //           value={text}
+  //           placeholder="Text"
+  //           onChange={(e) => setText(e.target.value)}
+  //           disabled={isSaving}
+  //         ></textarea>
+  //         <button
+  //           className="addentry__submit"
+  //           onClick={handleSubmit}
+  //           disabled={isSaving}
+  //         >
+  //           {isSaving ? "Saving..." : "Save"}
+  //         </button>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
 export default AddEntry;
